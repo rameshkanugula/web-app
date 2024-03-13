7ad3343c89f9e2404acc5ff25a05cf0a1171b2db
rameshkanugula <154396003+rameshkanugula@users.noreply.github.com>
2024-03-13 13:25:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker login -u rameshkanugula -p dckr_pat_lz8lgLxBeNawXwRaXof3ewr8blE
-docker tag capstone rameshkanugula/dev
+docker tag capstone1 rameshkanugula/dev
 docker push rameshkanugula/dev
 docker compose up -d
